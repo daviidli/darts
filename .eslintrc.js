74974402f55a23b7bba158df6cddd716bd3c7ecf
David Li <d.li.2247@gmail.com>
2020-04-01 00:13:47 -0700

    init commit

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+	extends: 'erb/typescript',
+	rules: {
+		// A temporary hack related to IDE not resolving correct package.json
+		'import/no-extraneous-dependencies': 'off',
+		'react/jsx-indent': [2, 'tab'],
+		'react/jsx-indent-props': [2, 'tab']
+	},
+	settings: {
+		'import/resolver': {
+			// See https://github.com/benmosher/eslint-plugin-import/issues/1396#issuecomment-575727774 for line below
+			node: {},
+			webpack: {
+				config: require.resolve('./configs/webpack.config.eslint.js')
+			}
+		}
+	}
+};
